51c77d73aa6716d7420554afc041de60e9cec965
Neal Satitsumpun <satitsumpun@wisc.edu>
2020-07-23 17:30:49 -0500

    Test

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.get('/', (req, res) => {
   console.log('Hello world received a request.');
 
   const target = process.env.TARGET || 'World';
-  res.send(`Hello ${target} github app!`);
+  res.send(`Hello ${target} github app with cloudbuild.yaml!`);
 });
 
 const port = process.env.PORT || 8080;
